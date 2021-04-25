50a98a44d7621561b3d7f471ccfcf544bae3f3a5
JDS <jeremysamaroo@gmail.com>
2021-04-25 06:08:59 -0300

    Update api routes

--- App/views/api.py
+++ App/views/api.py
@@ -5,3 +5,15 @@ api_views = Blueprint('api_views', __name__, template_folder='../templates')
 @api_views.route('/', methods=['GET'])
 def get_api_docs():
     return render_template('index.html')
+
+@api_views.route('/homepage', methods=['GET'])
+def home():
+    return render_template('homepage.html')
+
+@api_views.route('/list', methods=['GET'])
+def list():
+    return render_template('list.html')
+
+@api_views.route('/recipes', methods=['GET'])
+def recipes():
+    return render_template('recipes.html')
